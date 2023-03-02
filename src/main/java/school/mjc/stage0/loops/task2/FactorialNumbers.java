697f68c0ea07f3d28ba4ee3ce474204811f49bd2
Ivan Lapunka <ivan_lapunka@gmail.com>
2023-03-02 12:05:59 +0300

    Changes

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
-
+        for (int i = 0; i <= printToInclusive; i++) {
+            int mult = 1;
+            for (int j = 0; j <= i; j++) {
+                if (j == 0)
+                    continue;
+                mult *= j;
+            }
+            System.out.println(mult);
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
+        int i = 0;
+        int sgn = (int)Math.signum(multiplyByAndToInclusive);
+        multiplyByAndToInclusive *= sgn;
+        while (i <= multiplyByAndToInclusive) {
+            System.out.println(i * multiplyByAndToInclusive * sgn);
+            i++;
+        }
     }
 }
